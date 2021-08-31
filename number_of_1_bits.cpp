3b9acf3b6cf955f0593e892b58c8f8075a7b44e0
mubin86 <rahman.mmubinur@gmail.com>
2021-09-01 02:05:44 +0600

    no of 1 bits

---
+++ number_of_1_bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        for(int i = 0; i<32; i++){
+            int temp = n & 1;
+        
+            if(temp == 1){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+};
