aee5bff801213f194001ef07180de9eacbf6f115
mubin86 <rahman.mmubinur@gmail.com>
2021-09-13 02:56:28 +0600

    LCA

---
+++ lowest_common_ancestor.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(!root){
+            return NULL;
+        }
+         
+        if(root == p || root == q){
+            return root;
+        }
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        
+        if(l == NULL && r == NULL){
+            return NULL;
+        }
+         
+        if(l != NULL && r != NULL){
+            return root;
+        }
+        
+        if(!l){
+                return r;
+        }else{
+                return l;
+        }
+                 
+    }
+};
