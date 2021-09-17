124c80049458d51aafc97226b16da7132263274b
mubin86 <rahman.mmubinur@gmail.com>
2021-09-17 16:18:31 +0600

    power of three

---
+++ power_of_three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        long long int sum =1;
+        
+        while(sum <=n ){
+            sum *= 3;
+            
+            if(sum == n){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
