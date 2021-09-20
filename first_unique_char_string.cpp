07f1affcb7d61279a2d1047c3896389774a31792
mubin86 <rahman.mmubinur@gmail.com>
2021-09-21 01:02:45 +0600

    first unique char in string

---
+++ first_unique_char_string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int>m;
+        
+        for(int i= 0; i<s.length(); i++){
+            if(m.find(s[i]) == m.end()){
+                  m.insert({s[i], 1});
+            }else{
+                m.insert({s[i], m[s[i]]++});
+            }
+        }
+        
+        for(int i= 0; i<s.length(); i++){
+            if(m[s[i]] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
