68ae62d5f1cc3f732d6d38cee674e98cb17a37a7
mubin86 <rahman.mmubinur@gmail.com>
2022-01-14 23:15:34 +0600

    01 knapsack recursion

---
+++ 01_knapsack_recursion.cpp
@@ -0,0 +1,16 @@
+int knapsack(vector<int>wt, vector<int>val, int w, int n){
+
+    if(n==0 || w==0){
+        return 0;
+    }
+
+    if(wt[n-1] <= w){
+        return max( val[n-1] + knapsack(wt, val, w-wt[n-1], n-1),
+                        knapsack(wt, val, w, n-1)
+                   );
+    }
+    else{
+       return knapsack(wt, val, w, n-1);
+    }
+
+}
