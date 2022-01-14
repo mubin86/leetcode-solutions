84360bbbb5f33ba59012ed8f3a99041ff1c20f52
mubin86 <rahman.mmubinur@gmail.com>
2022-01-15 00:20:01 +0600

    01 knapsack recursion memoization

---
+++ 01_knapsack_recur_memoization.cpp
@@ -0,0 +1,24 @@
+int dp[101][1001];
+memset(dp, -1, sizeof(dp));
+
+int knapsack(vector<int>wt, vector<int>val, int w, int n){
+
+    if(n==0 || w==0){
+        return 0;
+    }
+
+    if(dp[n][w] != -1){
+        return dp[n][w];
+    }
+
+
+    if(wt[n-1] <= w){
+        return dp[n][w] = max( val[n-1] + knapsack(wt, val, w-wt[n-1], n-1),
+                        knapsack(wt, val, w, n-1)
+                   );
+    }
+    else{
+       return dp[n][w] = knapsack(wt, val, w, n-1);
+    }
+
+}
