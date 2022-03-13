7712a41a7d3dba90f437dfafeac1628337fda91a
mubin86 <rahman.mmubinur@gmail.com>
2022-03-13 23:40:44 +0600

    square sort array

---
+++ square_sort_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+     
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            nums[i] = pow(nums[i], 2);
+            
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        return nums;
+    }
+};
