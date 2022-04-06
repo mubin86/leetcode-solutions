47625ad7a60be68ee2fdce1ef9eb771703944106
mubin86 <rahman.mmubinur@gmail.com>
2022-04-06 23:47:22 +0600

    difference between sum and product of number

---
+++ diff_product_sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        
+        while(n != 0){
+            int temp = n%10;
+            
+            product *= temp;
+            sum += temp;
+            
+            n = n/10;
+            
+        }
+        
+        return product - sum;
+        
+    }
+};
