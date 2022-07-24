27452503af859e961757ef34edaed803f03baf22
mubin86 <rahman.mmubinur@gmail.com>
2022-07-24 23:38:13 +0600

    min operation to make array increasing

---
+++ min_operation_make_array_increasing.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int result = 0;
+        
+        for(int i = 1; i<nums.size(); i++){
+            
+            if(nums[i] <= nums[i-1]){
+                
+                int newNum = nums[i-1] + 1;
+                result += newNum - nums[i];
+                nums[i] = newNum; 
+            }
+        }
+        
+        return result;
+    }
+};
