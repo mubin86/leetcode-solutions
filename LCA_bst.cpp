ee6f23ab68eb046b603fa407460738a349555f71
mubin86 <rahman.mmubinur@gmail.com>
2022-08-13 16:20:53 +0600

    lowest common ancestor bst second approach

---
+++ LCA_bst.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    
+        if(root->val > max(p->val, q->val)){
+            return lowestCommonAncestor(root->left, p, q);
+            
+        }else if(root->val < min(p->val, q->val)){
+           return lowestCommonAncestor(root->right, p, q);
+            
+        }else{
+            return root;
+        }
+    }
+};
