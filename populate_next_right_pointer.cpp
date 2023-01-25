9ea1474b13dfcc711421bc9b90dba264aa2495e1
mubin86 <rahman.mmubinur@gmail.com>
2023-01-25 23:34:24 +0600

    populate next right pointer in each node

---
+++ populate_next_right_pointer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_map<int, Node*>m;
+    void populate(Node* root, int level){
+        if(!root){
+            return;
+        }
+
+        auto it = m.find(level);
+        if(it != m.end()){
+            it->second->next = root;
+            m[level] = root;
+            root->next = NULL;
+        }else{
+            m[level] = root;
+            root->next = NULL;
+        }
+
+        populate(root->left, level+1);
+        populate(root->right, level+1);
+    }
+
+    Node* connect(Node* root) {
+        populate(root, 0);
+        return root;
+    }
+};
