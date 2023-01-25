7bc8116f478bb6785d99be0455afdbdbb4b9998d
mubin86 <rahman.mmubinur@gmail.com>
2023-01-26 00:05:01 +0600

    optimized populate next right pointer

--- populate_next_right_pointer.cpp
+++ populate_next_right_pointer.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     unordered_map<int, Node*>m;
     void populate(Node* root, int level){
-        if(!root){
+       if(!root){
             return;
         }
 
         auto it = m.find(level);
         if(it != m.end()){
             it->second->next = root;
-            m[level] = root;
-            root->next = NULL;
-        }else{
-            m[level] = root;
-            root->next = NULL;
-        }
+        } 
+        m[level] = root;
 
         populate(root->left, level+1);
         populate(root->right, level+1);
