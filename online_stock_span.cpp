8ab87de3d55d938fb7cf61a573d20505cc1050c4
mubin86 <rahman.mmubinur@gmail.com>
2023-03-22 21:37:52 +0600

    online stock span

---
+++ online_stock_span.cpp
@@ -0,0 +1,23 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>>stk;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int ans=1;
+        while(stk.size() && stk.top().first <= price){
+            ans+=stk.top().second;
+            stk.pop();
+        }
+        stk.push({price,ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
