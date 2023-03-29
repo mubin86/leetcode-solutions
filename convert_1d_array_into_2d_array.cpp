a7fce3eb31fa22c9ef0b4d260f70204fd5d62207
mubin86 <rahman.mmubinur@gmail.com>
2023-03-29 16:42:38 +0600

    convert 1d array into 2d array

---
+++ convert_1d_array_into_2d_array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(m*n != original.size()){
+            return {};
+        }
+        vector<vector<int>>result(m, vector<int>(n));
+        int index = 0;
+        for(int i = 0; i<m; i++){
+            for(int j=0; j<n; j++){
+                result[i][j] = original[index];
+                index++;
+            }
+        }
+
+        return result;
+    }
+};
