4e9b9f7a3e8086d0b4ac33e5882c36b0cc07ad6b
mubin86 <rahman.mmubinur@gmail.com>
2023-06-22 18:31:11 +0600

    #design: a foodparking system

---
+++ design_a_food_rating_system.cpp
@@ -0,0 +1,36 @@
+class FoodRatings
+{
+public:
+    unordered_map<string, int> food_rating;
+    unordered_map<string, set<pair<int, string>>> cousin_rating;
+    unordered_map<string, string> food_cousin;
+
+    FoodRatings(vector<string> &foods, vector<string> &cuisines, vector< int > &ratings)
+    {
+        int n = foods.size();
+        for (int i = 0; i < n; i++)
+        {
+            food_rating[foods[i]] = ratings[i];
+            cousin_rating[cuisines[i]].insert({ -ratings[i], foods[i] });
+            food_cousin[foods[i]] = cuisines[i];
+        }
+    }
+
+    void changeRating(string food, int newRating)
+    {
+        int curr_rating = food_rating[food];
+
+        food_rating[food] = newRating;
+
+        string findcusine = food_cousin[food];
+       	
+        cousin_rating[findcusine].erase({ -curr_rating, food });
+
+        cousin_rating[findcusine].insert({ -newRating, food });
+    }
+
+    string highestRated(string cuisine)
+    {
+        return cousin_rating[cuisine].begin()->second;
+    }
+};
