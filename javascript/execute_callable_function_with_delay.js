ddd796f52b330ea0568a85e24919013ab3f0a564
mubin86 <rahman.mmubinur@gmail.com>
2023-06-27 01:42:56 +0600

    #JS: execute callable function with delay

---
+++ javascript/execute_callable_function_with_delay.js
@@ -0,0 +1,40 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let canceled = false;
+    
+    const timer = setTimeout(() => {
+        if (!canceled) {
+            fn(...args);
+        }
+    }, t);
+
+    const cancelFn = () => {
+        canceled = true;
+        clearTimeout(timer);
+    };
+
+    return cancelFn;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(fn, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
